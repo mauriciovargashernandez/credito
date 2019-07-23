3f13d2cb4a5dcde3c05581c71d1a945b74dbd9aa
Mauricio Vargas <mauriciovargashernandez@gmail.com>
2019-07-23 09:17:45 -0500

    Primer Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule  } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './core/login/login.component';
+
+const routes: Routes = [
+
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent }
+
+  // {
+  //   path: '',
+  //   loadChildren: () => {
+  //     return import('./components/components.module').then(m => m.ComponentsModule);
+  //   }
+  // }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginModule } from './login/login.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LoginModule
+  ]
+})
+export class CoreModule { }
